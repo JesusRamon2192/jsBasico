146a5d4ecdf47afb9e13d3648c34fc0405ebed77
jesusRamon <jesus.ramon2192@gmail.com>
2022-12-21 18:20:18 -0600

    Construyendo tree

---
+++ curso2/queue.js
@@ -0,0 +1,45 @@
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.length = 0;
+    }
+    enqueue(value){
+        const newNode = new Node(value);
+        if(this.length === 0){
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        this.length++;
+        return this;
+    }
+    dequeue (){
+        //Eliminar el primer elemento
+        if(this.length ===0){
+            const msg = "No hay elementos en su queue";
+            return msg;
+        } else if ( this.length === 1){
+            this.first = null;
+            this.last = null;
+        }else {
+            this.first = this.first.next;
+        }
+        this.length--;
+        return this;
+    }
+    peek(){
+        return this.first;
+    }  
+}
+
+const myQueue = new Queue();
